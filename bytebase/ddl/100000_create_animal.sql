1710d7dfc398cd99fd52c47c0905ecba723155b4
Vishal <vishalchauhan3549@gmail.com>
2024-06-12 15:21:35 +0530

    Made new tables;

---
+++ bytebase/ddl/100000_create_animal.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Animals (
+    AnimalID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    Species VARCHAR(50) NOT NULL,
+    DateOfBirth DATE,
+    Gender CHAR(1),
+    Habitat VARCHAR(100)
+);
