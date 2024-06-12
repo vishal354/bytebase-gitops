f580dc66c44afe53b97182ea9b50caaf153c1a83
Vishal <vishalchauhan3549@gmail.com>
2024-06-12 10:25:49 +0530

    Adding a new table

---
+++ bytebase/100_create_t1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    DateOfBirth DATE,
+    Gender CHAR(1),
+    Email VARCHAR(100) UNIQUE,
+    Major VARCHAR(100)
+);
